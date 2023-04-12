69519f2afaf675c40cc2f8e997b4209d60c67777
gromitmugs <jediacp2000@gmail.com>
2023-04-11 20:39:47 -0700

    differential drive

---
+++ scripts/bringup/diff_tf.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+
+"""
+   diff_tf.py - follows the output of a wheel encoder and
+   creates tf and odometry messages.
+   some code borrowed from the arbotix diff_controller script
+   A good reference: http://rossum.sourceforge.net/papers/DiffSteer/
+   
+    Copyright (C) 2012 Jon Stephan. 
+     
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   
+   ----------------------------------
+   Portions of this code borrowed from the arbotix_python diff_controller.
+   
+diff_controller.py - controller for a differential drive
+  Copyright (c) 2010-2011 Vanadium Labs LLC.  All right reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of Vanadium Labs LLC nor the names of its 
+        contributors may be used to endorse or promote products derived 
+        from this software without specific prior written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL VANADIUM LABS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+import rospy
+import roslib
+roslib.load_manifest('differential_drive')
+from math import sin, cos, pi
+
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.broadcaster import TransformBroadcaster
+from std_msgs.msg import Float64MultiArray
+
+
+#############################################################################
+class DiffTf:
+#############################################################################
+
+    #############################################################################
+    def __init__(self):
+    #############################################################################
+        rospy.init_node("diff_tf")
+        self.nodename = rospy.get_name()
+        rospy.loginfo("-I- %s started" % self.nodename)
+        
+        #### parameters #######
+        self.rate = rospy.get_param('~rate',10.0)  # the rate at which to publish the transform
+        self.ticks_per_rev = float(rospy.get_param('~ticks_meter', 60))  # The number of wheel encoder ticks per meter of travel
+        self.base_width = float(rospy.get_param('~base_width', 0.245)) # The wheel base width in meters
+        
+        self.base_frame_id = rospy.get_param('~base_frame_id','base_link') # the name of the base frame of the robot
+        self.odom_frame_id = rospy.get_param('~odom_frame_id', 'odom') # the name of the odometry reference frame
+ 
+        self.wheel_radius = float(rospy.get_param('~wheel_radius', 0.035))
+
+        self.t_delta = rospy.Duration(1.0/self.rate)
+        self.t_next = rospy.Time.now() + self.t_delta
+
+        
+        
+        # internal data
+        self.d_left = 0
+        self.d_right = 0
+        self.v_left = 0
+        self.v_right = 0
+
+        self.x = 0                  # position in xy plane 
+        self.y = 0
+        self.th = 0
+        self.dx = 0                 # speeds in x/rotation
+        self.dr = 0
+        self.then = rospy.Time.now()
+        
+        # subscriptions
+        rospy.Subscriber("/raw_vel", Float64MultiArray, self.rawVelCallback)
+        rospy.Subscriber("/raw_pos", Float64MultiArray, self.rawPosCallback)
+
+        self.odomPub = rospy.Publisher("/odom", Odometry, queue_size=10)
+        self.odomBroadcaster = TransformBroadcaster()
+        
+    #############################################################################
+    def spin(self):
+    #############################################################################
+        r = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            self.update()
+            r.sleep()
+       
+     
+    #############################################################################
+    def update(self):
+    #############################################################################
+        now = rospy.Time.now()
+        if now > self.t_next:
+            elapsed = now - self.then
+            self.then = now
+            elapsed = elapsed.to_sec()
+    
+           
+            # distance traveled is the average of the two wheels 
+            d = ( self.d_left + self.d_right ) / 2
+            # this approximation works (in radians) for small angles
+            th = ( self.d_right - self.d_left ) / self.base_width
+            # calculate velocities
+            self.dx = d / elapsed
+            self.dr = th / elapsed
+           
+             
+            if (d != 0):
+                # calculate distance traveled in x and y
+                x = cos( th ) * d
+                y = -sin( th ) * d
+                # calculate the final position of the robot
+                self.x = self.x + ( cos( self.th ) * x - sin( self.th ) * y )
+                self.y = self.y + ( sin( self.th ) * x + cos( self.th ) * y )
+            if( th != 0):
+                self.th = self.th + th
+                
+            # publish the odom information
+            quaternion = Quaternion()
+            quaternion.x = 0.0
+            quaternion.y = 0.0
+            quaternion.z = sin( self.th / 2 )
+            quaternion.w = cos( self.th / 2 )
+            self.odomBroadcaster.sendTransform(
+                (self.x, self.y, 0),
+                (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
+                rospy.Time.now(),
+                self.base_frame_id,
+                self.odom_frame_id
+                )
+            
+            odom = Odometry()
+            odom.header.stamp = now
+            odom.header.frame_id = self.odom_frame_id
+            odom.pose.pose.position.x = self.x
+            odom.pose.pose.position.y = self.y
+            odom.pose.pose.position.z = 0
+            odom.pose.pose.orientation = quaternion
+            odom.child_frame_id = self.base_frame_id
+            odom.twist.twist.linear.x = self.dx
+            odom.twist.twist.linear.y = 0
+            odom.twist.twist.angular.z = self.dr
+            self.odomPub.publish(odom)
+            
+            
+
+
+    #############################################################################
+    def rawVelCallback(self, msg):
+    #############################################################################
+        v_left = msg.data[0]
+        v_right = msg.data[1]
+        
+    #############################################################################
+    def rawPosCallback(self, msg):
+    #############################################################################
+        tick_left = msg.data[0]
+        tick_right = msg.data[1]
+        self.d_left = 2 * pi * self.wheel_radius * (tick_left / self.ticks_per_rev)
+        self.d_right = 2 * pi * self.wheel_radius * (tick_right / self.ticks_per_rev)
+
+#############################################################################
+#############################################################################
+if __name__ == '__main__':
+    """ main """
+    try:
+        diffTf = DiffTf()
+        diffTf.spin()
+    except rospy.ROSInterruptException:
+        pass
---
+++ scripts/bringup/wheel_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import rospy
+import odrive
+import math
+from odrive.enums import *
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64MultiArray
+import sys
+
+WHEEL_DIST = 0.52 #in meters
+TYRE_CIRCUMFERENCE = 2*math.pi*0.035
+
+vel_pub = rospy.Publisher('/raw_vel', Float64MultiArray, queue_size=10)
+pos_pub = rospy.Publisher('/raw_pos', Float64MultiArray, queue_size=10)
+raw_vel = Float64MultiArray()
+raw_pos = Float64MultiArray()
+vel = [0.0,0.0]
+pos = [0.0,0.0]
+
+rospy.init_node('odrive')
+rospy.loginfo("Connecting to odrive...")
+odrv0 = odrive.find_any()
+rospy.loginfo("Initialise motors...")
+odrv0.axis0.controller.input_vel = 0
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
+odrv0.axis1.controller.input_vel = 0
+odrv0.axis1.requested_state = AXIS_STATE_IDLE    
+
+def start_odrive():
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        try:
+            try:
+            # rospy.loginfo('{}'.format(vel))
+                vel[0] = float(-1*odrv0.axis0.encoder.vel_estimate * TYRE_CIRCUMFERENCE) #vel left
+                vel[1] = float(odrv0.axis1.encoder.vel_estimate * TYRE_CIRCUMFERENCE) #vel right
+                pos[0] = -1*odrv0.axis0.encoder.pos_estimate_counts #pos left
+                pos[1] = odrv0.axis1.encoder.pos_estimate_counts #pos right
+            except:
+                pass
+            raw_vel.data = tuple(vel)
+            raw_pos.data = tuple(pos)
+            vel_pub.publish(raw_vel)
+            pos_pub.publish(raw_pos)
+            rate.sleep()
+        except KeyboardInterrupt:
+            odrive_abort()
+
+        
+        
+def odrive_abort():
+    odrv0.axis0.controller.input_vel = 0
+    odrv0.axis0.requested_state = AXIS_STATE_IDLE
+    odrv0.axis1.controller.input_vel = 0
+    odrv0.axis1.requested_state = AXIS_STATE_IDLE
+    odrv0.reboot()
+    quit()
+
+if __name__ == '__main__':
+    try:
+        rospy.loginfo("Done...")
+        start_odrive()
+    except rospy.ROSInterruptException:
+        pass
