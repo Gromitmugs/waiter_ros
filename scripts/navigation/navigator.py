7ae203b02abc05e53643aac9b7651ef9a9200a53
pooh8rw <pooh8rw@gmail.com>
2023-04-20 12:40:22 +0700

    added code for pump and navigation under pump.py and navigator.py

---
+++ scripts/navigation/navigator.py
@@ -0,0 +1,120 @@
+import actionlib
+import rospy
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Point, Pose, Quaternion
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+# Data structure: receive location command with pos data type. (positions and quarternions)
+# Topic nav_location:
+#                   location:
+#                           name: kitchen
+#                           position: x, y
+#                           orientation: r1, r2, r3, r4
+#                   location:  
+#                           name: table
+#                           position: x, y
+#                           orientation: r1, r2, r3, r4
+
+class Navigator():
+
+    def __init__(self):
+
+        self.goal_sent = False
+        
+        rospy.Subscriber("nav_location", Pose, self.callback)
+
+        # What to do if shut down (e.g. Ctrl-C or failure)
+        rospy.on_shutdown(self.shutdown)
+
+        # Tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient(
+            "move_base", MoveBaseAction)
+        rospy.loginfo("Wait for the action server to come up")
+
+        # Allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+
+    def callback(self, navLocation):
+        rospy.set_param('ROBOT_STATUS', '1') # assuming 1 is not free
+        print("NAVIGATING!")
+        if navLocation.name == 'KITCHEN' :
+            position = {'x': navLocation.position.x,
+                    'y': navLocation.position.y}
+            quaternion = {'r1':navLocation.orientation.x,'r2':navLocation.orientation.y, 'r3':navLocation.orientation.z, 'r4':navLocation.orientation.w}
+            rospy.loginfo("Going to %s at (%s, %s) pose", navLocation.name, position['x'], position['y'])
+            success = self.goto(position, quaternion)
+
+            if success:
+                rospy.loginfo("Successfully Reached {0}".format(navLocation.name))
+            else:
+                rospy.loginfo("The base failed to reach the desired pose at destination")
+
+        elif navLocation.name == 'TABLE' :
+            position = {'x': navLocation.position.x,
+                    'y': navLocation.position.y}
+            quaternion = {'r1':navLocation.orientation.x,'r2':navLocation.orientation.y, 'r3':navLocation.orientation.z, 'r4':navLocation.orientation.w}
+            rospy.loginfo("Going to %s at (%s, %s) pose", navLocation.name, position['x'], position['y'])
+            success = self.goto(position, quaternion)
+
+            if success:
+                rospy.loginfo("Successfully Reached {0}".format(navLocation.name))
+            else:
+                rospy.loginfo("The base failed to reach the desired pose at destination")
+
+        else:
+            print("The base failed to reach the desired pose")
+        
+        print("NAVIGATED!")
+        rospy.set_param('ROBOT_STATUS', '0') # assuming 0 is free
+
+        
+
+    def goto(self, pos, quat):
+
+        # Send a goal
+        self.goal_sent = True
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+
+        # Start moving
+        self.move_base.send_goal(goal)
+
+        # Allow TurtleBot up to 60 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(60))
+
+        state = self.move_base.get_state()
+        result = False
+
+        if success and state == GoalStatus.SUCCEEDED:
+            # We made it!
+            result = True
+        else:
+            self.move_base.cancel_goal()
+
+        self.goal_sent = False
+        return result
+
+    def shutdown(self):
+        if self.goal_sent:
+            self.move_base.cancel_goal()
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        #rospy.init_node('navigator_listener', anonymous=True)
+        rospy.init_node('nav_test', anonymous=False)
+        navigator = Navigator()
+
+        
+
+        # Sleep to give the last log messages time to be sent
+        rospy.sleep(1)
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Ctrl-C caught. Quitting")
